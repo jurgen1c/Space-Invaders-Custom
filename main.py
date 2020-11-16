bd07c820fbb516bf6d6b12c2b78de710e0bd5c2e
jurgen1c <jurgen1c@gmail.com>
2020-11-16 11:46:15 -0600

    Setup imports & assets

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+import os
+import time
+import random
+
+# Load images
+RED_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'pixel_ship_red_small.png'))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'pixel_ship_green_small.png'))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'pixel_ship_blue_small.png'))
+
+# Player ship
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'pixel_ship_yellow.png'))
+
+# Lasers
+
+RED_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_red.png'))
+GREEN_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_green.png'))
+BLUE_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_blue.png'))
+YELLOW_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_yellow.png'))
+
+# Background
+BG = pygame.image.load(os.path.join('assets', 'background-black.png'))
