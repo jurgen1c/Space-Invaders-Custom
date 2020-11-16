6ec890d83364219c70aea76a6295eeb031756094
jurgen1c <jurgen1c@gmail.com>
2020-11-16 12:13:27 -0600

    Setup window & events

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import pygame
 import os
 import time
 import random
+pygame.font.init()
+
+WIDTH, HEIGHT = 750, 650
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Space Invaders Custom')
 
 # Load images
 RED_SPACE_SHIP = pygame.image.load(os.path.join('assets', 'pixel_ship_red_small.png'))
@@ -19,4 +24,34 @@ BLUE_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_blue.png'))
 YELLOW_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_yellow.png'))
 
 # Background
-BG = pygame.image.load(os.path.join('assets', 'background-black.png'))
+BG = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'background-black.png')), (WIDTH, HEIGHT))
+
+def main():
+  run = True
+  FPS = 60
+  level = 1
+  lives = 5
+  main_font = pygame.font.SysFont('comicsans', 50)
+  clock = pygame.time.Clock()
+
+  def redraw_window():
+    WIN.blit(BG, (0,0))
+
+    #draw text
+    lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
+    level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
+
+    WIN.blit(lives_label, (10,10))
+    WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
+    pygame.display.update()
+
+
+  while run:
+    clock.tick(FPS)
+    redraw_window()
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        run = False
+
+main()
